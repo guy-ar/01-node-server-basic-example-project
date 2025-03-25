390b9d5a7c5017a43f373b66642c470906a20f7b
Guy Arbilly <Guy.Arbilly@gmail.com>
2025-03-25 18:03:45 +0200

    section 3 - chapter 35 - parse request body and save it to file

--- app.js
+++ app.js
@@ -1,21 +1,9 @@
 const http = require('http');
 const fs = require('fs');
-// first alternaltive - create function for request listener
-//and then use it for create server
-// function requestHandler(req, res) {
 
-// }
-
-// http.createServer(requestHandler);
-
-// same can be achived with arrow function
-// http.createServer((req, res) => {
-//     ...
-// });
 
 const server = http.createServer((req, res) => {
-    //console.log(req.url, req.method, req.headers);
-    //process.exit();
+    
     const url = req.url;
     // check for the url
     if (url === '/') {
@@ -27,7 +15,26 @@ const server = http.createServer((req, res) => {
         return res.end();
     }
     if (url === '/message' && req.method === 'POST') {
-        fs.writeFileSync('message.txt', 'DUMMY');
+        const body = [];
+        // register event listener on data event = will be fired when new buffer is ready to be read
+        // within defined function, we can access the buffer
+        req.on('data', (chunk) => {
+            // add new chunk to the body array
+            body.push(chunk);
+            console.log(chunk);
+        });
+
+        // register event listener on end event = will be fired when all data has been read
+        req.on('end', () => {
+            // create new buffer and concatenate all chunks into one
+            const parsedBody = Buffer.concat(body).toString();
+            console.log(parsedBody);
+            // extract message from parsedBody - witout the property name
+            const message = parsedBody.split('=')[1];
+            // write message to a file
+            fs.writeFileSync('message.txt', message);
+        });
+
         // redirect user to the root page
         res.statusCode = 302;
         // default header that is accepted by the browser
@@ -35,14 +42,6 @@ const server = http.createServer((req, res) => {
         return res.end();
     }
 
-    // attach header to response
-    res.setHeader('Content-Type', 'text/html');
-    // write response
-    res.write('<html>');
-    res.write('<head><title>My first page</title></head>');
-    res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
-    res.write('</html>');
-    res.end();
 });
 
 // start the server and listen on port 3000 to incoming requests
